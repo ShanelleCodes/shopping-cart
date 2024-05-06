770dc575e58922d1601bc05b08e77f70abbafcf6
Shanelle Codes <shanelle.codes@gmail.com>
2024-05-06 09:55:24 -0400

    homepage slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import "./Slider.scss"
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+const Slider = () => {
+const [currentSlide, setCurrentSlide] = useState(0);
+
+const data = [
+    "/img/charlesdeluvio-eR8qaAM6k1U-unsplash.jpg",
+    "/img/pexels-daniel-adesina-279287-833052 (1).jpg",
+    "/img/pexels-foi-international-351919-1036396 (1).jpg",
+    
+];
+
+const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
+
+  return (
+    <div className="slider">
+      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <NavigateBeforeIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <NavigateNextIcon />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Slider
